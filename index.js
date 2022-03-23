893e83d615687ced913e4f95f6fb4cfedd15f5d1
Johnathan <jocuog@gmail.com>
2022-03-22 23:18:51 -0600

    commit tree 5c1f625607a8706180c878d3a44d0b97b0108243

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+const submitData = (name, email) => {
+
+const obj = {
+    name: `${name}`,
+    email: `${email}`
+}
+
+const config = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json'
+    },
+    body: JSON.stringify(obj)
+};
+    return fetch("http://localhost:3000/users", config)
+    .then((r) => r.json())
+    .then((rep) => {
+        const value = rep.id;
+        const p = document.createElement('p')
+        p.textContent = value;
+        const body = document.querySelector('body');
+        body.append(p);
+    })
+
+    .catch(error => {
+        const message = document.createElement('p')
+        message.textContent = error.message;
+        const body = document.querySelector('body');
+        body.append(message);
+    })
+
+}
